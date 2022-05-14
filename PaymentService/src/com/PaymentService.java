53164ab7dd7010dcf2b535b198d76efeee6fcd57
IT20138622 <it20138622@my.sliit.lk>
2022-05-15 00:08:28 +0530

    implement doDelete method

--- PaymentService/src/com/PaymentService.java
+++ PaymentService/src/com/PaymentService.java
@@ -66,5 +66,18 @@ public class PaymentService extends HttpServlet {
 		 
 		 			response.getWriter().write(output);
 	}
+	
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		Map paras = getParasMap(request); 
+		 
+		 String output = payObj.deletePayment(paras.get("PaymentID").toString());  
+		 
+		 response.getWriter().write(output);
+	}
   
 }
