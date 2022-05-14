09ba000a2fa69b8b9d9c649de7edf9079f536395
IT20138622 <it20138622@my.sliit.lk>
2022-05-15 00:06:19 +0530

    create doGet method

--- PaymentService/src/com/PaymentService.java
+++ PaymentService/src/com/PaymentService.java
@@ -1,6 +1,17 @@
 package com;
 
+import com.Payment;
+import java.io.IOException; 
+import java.util.HashMap; 
+import java.util.Map; 
+import java.util.Scanner;
 
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 /**
@@ -18,3 +29,13 @@ public class PaymentService extends HttpServlet {
        super();
        // TODO Auto-generated constructor stub
    }
+   
+   /**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+  
+}
