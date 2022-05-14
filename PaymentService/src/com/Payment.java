3d2fb38de6c0e77a77322f304fb49c6133e63fde
IT20138622 <it20138622@my.sliit.lk>
2022-05-14 23:59:54 +0530

    create insert method

--- PaymentService/src/com/Payment.java
+++ PaymentService/src/com/Payment.java
@@ -24,5 +24,46 @@ public class Payment {
 		}
 		return con;
 	}
+	
+	//insert
+
+		public String insertPayment(String CustomerName, String BillAmount, String AccountNo)  
+		{   
+			String output = ""; 
+		 
+			try   
+			{    
+				Connection con = connect(); 
+		 
+				if (con == null)    
+				{return "Error while connecting to the database for inserting."; } 
+		 
+				// create a prepared statement 
+				String query = " insert into pay (PaymentID , CustomerName , BillAmount , AccountNo)"+ " values (?, ?, ?, ?)"; 
+		 
+		 
+				PreparedStatement preparedStmt = con.prepareStatement(query); 
+		 
+				// binding values    
+				 preparedStmt.setInt(1, 0);
+				 preparedStmt.setString(2, CustomerName);
+				 preparedStmt.setString(3, BillAmount);
+				 preparedStmt.setString(4, AccountNo);
+				 
+				
+				// execute the statement    
+				preparedStmt.execute();    
+				con.close(); 
+		   
+				String newPayment = readPayment(); 
+				output =  "{\"status\":\"success\", \"data\": \"" + newPayment + "\"}";    
+			}   
+			catch (Exception e)   
+			{    
+				output =  "{\"status\":\"error\", \"data\": \"Error while inserting the Payment.\"}";  
+				System.err.println(e.getMessage());   
+			} 
+			
+		  return output;  
 
 }
