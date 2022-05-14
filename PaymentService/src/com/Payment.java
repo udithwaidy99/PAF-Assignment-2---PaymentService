26c5286e22cd46f74a39195eb5dc5552c36fdccb
IT20138622 <it20138622@my.sliit.lk>
2022-05-14 23:58:26 +0530

    create database connection

--- PaymentService/src/com/Payment.java
+++ PaymentService/src/com/Payment.java
@@ -1,5 +1,28 @@
 package com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+
+//data base connection
+
 public class Payment {
+	
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/lab", "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 
 }
