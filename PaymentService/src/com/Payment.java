76b403e9bd73a5145bfde94e35538e024b3db7a5
IT20138622 <it20138622@my.sliit.lk>
2022-05-15 00:00:56 +0530

    create read method

--- PaymentService/src/com/Payment.java
+++ PaymentService/src/com/Payment.java
@@ -65,5 +65,66 @@ public class Payment {
 			} 
 			
 		  return output;  
+		  
+		}
+		
+		//read
+		
+		public String readPayment()  
+		{   
+			String output = ""; 
+		
+			try   
+			{    
+				Connection con = connect(); 
+			
+				if (con == null)    
+				{
+					return "Error while connecting to the database for reading."; 
+				} 
+		 
+				// Prepare the html table to be displayed    
+				output = "<table border=\'1\'><tr><th>CustomerName</th><th>BillAmount</th><th>AccountNo</th><th>Update</th><th>Remove</th></tr>";
+		 
+				String query = "select * from pay"; 
+				Statement stmt = con.createStatement(); 
+				ResultSet rs = stmt.executeQuery(query); 
+		 
+				// iterate through the rows in the result set    
+				while (rs.next())    
+				{     
+					String PaymentID = Integer.toString(rs.getInt("PaymentID")); 
+					 String CustomerName = rs.getString("CustomerName");
+					 String BillAmount = rs.getString("BillAmount");
+					 String AccountNo = rs.getString("AccountNo");
+					 
+		 
+					// html table values
+					output += "<tr><td><input id=\'hidPaymentIDUpdate\' name=\'hidPaymentIDUpdate\' type=\'hidden\' value=\'" + PaymentID + "'>" 
+								+ CustomerName + "</td>"; 
+					output += "<td>" + BillAmount + "</td>";
+					output += "<td>" + AccountNo + "</td>";
+					
+
+					  
+		 
+					// table buttons     
+					output +="<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary'></td>"       
+							+ "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-iid='" + PaymentID + "'>" + "</td></tr>"; 
+				
+				}
+				con.close(); 
+		 
+				// Complete the html table    
+				output += "</table>";   
+			}   
+			catch (Exception e)   
+			{    
+				output = "Error while reading the Payment.";    
+				System.err.println(e.getMessage());   
+			} 
+		 
+			return output;  
+		}
 
 }
