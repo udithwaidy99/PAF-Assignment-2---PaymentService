b2fec33d6cb129f92ea426b243809f874094fb14
IT20138622 <it20138622@my.sliit.lk>
2022-05-15 00:08:56 +0530

    Convert request parameters to a Map

--- PaymentService/src/com/PaymentService.java
+++ PaymentService/src/com/PaymentService.java
@@ -79,5 +79,30 @@ public class PaymentService extends HttpServlet {
 		 
 		 response.getWriter().write(output);
 	}
+	
+	// Convert request parameters to a Map
+			private static Map getParasMap(HttpServletRequest request)
+			{
+			 Map<String, String> map = new HashMap<String, String>();
+			try
+			 { 
+			 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			 String queryString = scanner.hasNext() ?
+			 scanner.useDelimiter("\\A").next() : "";
+			 scanner.close();
+			 String[] params = queryString.split("&");
+			 for (String param : params)
+			 { 
+			
+			String[] p = param.split("=");
+			 map.put(p[0], p[1]);
+			 }
+			 }
+			catch (Exception e)
+			 {
+			 }
+			return map;
+			}
+			
   
 }
